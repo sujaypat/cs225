97441c663254627c4debf4b1b00e0587b1637c9a
ptwrdhn2 <ptwrdhn2@371cef98-3a89-4023-b202-dd5c91b28d9f>
2016-01-28 02:49:41 +0000

    should be working

--- ptwrdhn2/mp2/image.cpp
+++ ptwrdhn2/mp2/image.cpp
@@ -1,25 +1,58 @@
+#include "image.h"
 
+void Image::flipleft(){
 
-class Image : public PNG{
+  int width = (*this).width();
+  int height = (*this).height();
+  RGBAPixel currPix;
 
-  void flipleft(){
+  for(int i = 0; i < width / 2; i++){
+    for(int j = 0; j < height; j++){
+	
+      currPix = *(*this)(i, j);
+      *(*this)(i, j) = *(*this)(width - 1 - i, j);
+      *(*this)(width - 1 - i, j) = currPix;
 
-
-
-  }
-
-  void adjustbrightness(int r, int g, int b){
-
-
-
-
-  }
-
-  void invertcolors(){
+    }
     
+  }
 
+}
 
+void Image::adjustbrightness(int r, int g, int b){
+
+  int width = (*this).width();
+  int height = (*this).height();
+
+  for(int i = 0; i < width; i++){
+    for(int j = 0; j < height; j++){
+	
+      RGBAPixel *currPix = (*this)(i,j);
+      currPix->red = currPix->red + r > 255 ? 255 : currPix->red + r < 0 ? 0 : currPix->red + r; 
+      currPix->green = currPix->green + g > 255 ? 255 : currPix->green + g < 0 ? 0 : currPix->green + g; 
+      currPix->blue = currPix->blue + b > 255 ? 255 : currPix->blue + b < 0 ? 0 : currPix->blue + b;
+
+    }
+   
+  }
+
+}
+
+void Image::invertcolors(){
+    
+  int width = this->width();
+  int height = this->height();
+    
+  for (int i = 0; i < width; i++){
+    for (int j = 0; j < height; j++){
+
+      RGBAPixel *currPix = (*this)(i,j);
+      currPix->red = 255-currPix->red;
+      currPix->green = 255-currPix->green;
+      currPix->blue = 255-currPix->blue;
+
+    }
 
   }
 
-};
+}
