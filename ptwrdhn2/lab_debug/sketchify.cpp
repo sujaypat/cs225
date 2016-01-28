7f309ac7e049d67a0103bfe1485510e87393fe6c
ptwrdhn2 <ptwrdhn2@371cef98-3a89-4023-b202-dd5c91b28d9f>
2016-01-28 03:15:42 +0000

    idk some scrub didn't debug

--- ptwrdhn2/lab_debug/sketchify.cpp
+++ ptwrdhn2/lab_debug/sketchify.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
+#include <iostream>
 #include "png.h"
-
+using namespace std;
 //
 // sets up the output image
 PNG* setupOutput(int w, int h)
@@ -12,33 +13,33 @@ PNG* setupOutput(int w, int h)
 // Returns my favorite color
 RGBAPixel* myFavoriteColor(int intensity)
 {
-    RGBAPixel color;
-    color.red   = 0;
-    color.green = intensity / 2;
-    color.blue  = intensity;
-    return &color;
+    RGBAPixel *color = new RGBAPixel;
+    color->red   = 0;
+    color->green = intensity / 2;
+    color->blue  = intensity;
+    return color;
 }
 
 void sketchify()
 {
     // Load in.png
-    PNG* original;
+    PNG* original = new PNG;
 
     original->readFromFile("in.png");
     int width = original->width();
     int height = original->height();
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = new PNG;
+    output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
     RGBAPixel* myPixel = myFavoriteColor(192);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (int y = 1; 0 < y < height; y++) {
-        for (int x = 1; 0 < x < width; x++) {
+    for (int y = 1; y < height; y++) {
+        for (int x = 1; x < width; x++) {
             // Calculate the pixel difference
             RGBAPixel* prev = (*original)(x - 1, y - 1);
             RGBAPixel* curr = (*original)(x, y);
@@ -50,7 +51,7 @@ void sketchify()
             // color the output pixel with my favorite color
             RGBAPixel* currOutPixel = (*output)(x, y);
             if (diff > 100)
-                currOutPixel = myPixel;
+                *currOutPixel = *myPixel;
         }
     }
 
