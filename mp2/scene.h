a81bba12a7ca7e490e15f771a743675b5d49a666
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-01-30 21:23:31 +0000

    seems to be working

--- mp2/scene.h
+++ mp2/scene.h
@@ -6,6 +6,7 @@
 
 class Scene{
  public:
+
   Scene(int max);
 
   ~Scene();
@@ -15,27 +16,27 @@ class Scene{
   const Scene & operator= (const Scene &source);
 
   void changemaxlayers(int newmax);
-        
+
   void addpicture (const char *FileName, int index, int x, int y);
         
   void changelayer (int index, int newindex);
         
   void translate(int index, int xcoord, int ycoord);
- 
+        
   void deletepicture(int index);
-
+       
   Image * getpicture ( int index) const;
-
+        
   Image drawscene() const;
         
  private:
 
   int max;
   Image ** layers; 
-  int * xcoords;
-  int * ycoords;
+  int * xvals;
+  int * yvals;
   void _copy(const Scene & other);
   void _clear();
 };
 
-#endif
+#endif //SCENE_H
