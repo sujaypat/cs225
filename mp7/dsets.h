2f6d3e704402152bab8dc41f8068bfb256201651
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-04-24 21:48:24 +0000

    7.1 probably working

--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,20 @@
 /* Your code here! */
+#ifndef DSETS_H
+#define DSETS_H
+#include <vector>
+
+using namespace std;
+
+class DisjointSets {
+
+    public:
+	    void addelements(int num);
+	    int find (int elem);
+	    void setunion(int a, int b);
+    	bool isBigger(int c, int d);
+
+    private:
+    	vector<int> lmao;
+};
+
+#endif
