24aef914beb5a2ab023e5d653eb1af5aac5b4e61
ptwrdhn2 <ptwrdhn2@371cef98-3a89-4023-b202-dd5c91b28d9f>
2016-04-29 21:43:45 +0000

    lab graphs

--- ptwrdhn2/lab_graphs/graph_tools.h
+++ ptwrdhn2/lab_graphs/graph_tools.h
@@ -22,26 +22,26 @@ using std::unordered_map;
  *  operations on the Graph class. Your assignment this lab
  *  will be to implement these three functions.
  */
-namespace GraphTools
-{
+namespace GraphTools{
 
     /**
      * Finds the minimum edge weight in the Graph graph.
      */
-    int findMinWeight(Graph& graph);
+     int findMinWeight(Graph & graph);
 
     /**
      * Returns the shortest distance (in edges) between the Vertices
      *  start and end.
      */
-    int findShortestPath(Graph& graph, Vertex start, Vertex end);
+    int findShortestPath(Graph & graph, Vertex start, Vertex end);
 
     /**
      * Finds a minimal spanning tree on a graph.
      */
-    void findMST(Graph& graph);
+    void findMST(Graph & graph);
 
     // define any helper functions here:
+	void minWeightHelper(Graph & graph, Vertex v, stack<Vertex>& vertices1, stack<Vertex>& vertices2);
 
 }
 
